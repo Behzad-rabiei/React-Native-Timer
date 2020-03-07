b81381428428fe15252b6a2339d98e5e4320f673
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-07 20:53:51 +0330

    Inital implementation for Home.js

---
+++ App/screens/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Container from "../components/container";
+
+class Home extends Component {
+  render() {
+    return <Container />;
+  }
+}
+
+export default Home;
