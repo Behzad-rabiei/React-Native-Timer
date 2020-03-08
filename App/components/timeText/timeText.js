70bd797e3aeb8a68fbd05bed9899bb2425ba79db
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-08 14:47:25 +0330

    formating text for TimeText component

--- App/components/timeText/timeText.js
+++ App/components/timeText/timeText.js
@@ -3,8 +3,30 @@ import { Text } from "react-native";
 import styles from "./styles";
 
 class TimeText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      remainingSeconds: 5
+    };
+  }
+
+  getRemaining = time => {
+    const minutes = Math.floor(time / 60);
+    const secondes = time - minutes * 60;
+    return {
+      minutes: this.formatNumber(minutes),
+      secondes: this.formatNumber(secondes)
+    };
+  };
+
+  formatNumber = number => `0${number}`.slice(-2);
+
   render() {
-    return <Text style={styles.text}>00:07</Text>;
+    const { secondes, minutes } = this.getRemaining(
+      this.state.remainingSeconds
+    );
+    const time = `${minutes}:${secondes}`;
+    return <Text style={styles.text}>{time}</Text>;
   }
 }
 
