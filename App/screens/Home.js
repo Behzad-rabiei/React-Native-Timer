8624107b04cd2ec8d0825293e49d63ad3f385267
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-08 12:14:06 +0330

    Home.js updated

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import { StatusBar } from "react-native";
 import { Container } from "../components/container";
 import { StartButton } from "../components/startButton.js";
+import { TimeText } from "../components/timeText";
 
 class Home extends Component {
   render() {
     return (
       <Container>
+        <StatusBar barStyle="light-content" />
+        <TimeText />
         <StartButton />
       </Container>
     );
